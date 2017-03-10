7fb31de9b0531c67b9a2bab9ec4219f38513bc57
sᴀʟᴠᴀᴛᴏʀᴇ <svt.busiello@gmail.com>
2017-03-10 01:29:13 +0100

    Implemented new context_processor Now you can access to alternate url for your current page with

--- context_processors.py
+++ context_processors.py
@@ -1,7 +1,24 @@
 from django.conf import settings as django_settings
+from django.core.urlresolvers import resolve, reverse
+from django.utils.translation import activate, get_language
 
 def settings(request):
-    # dict_settings = dict()
-    # for key in django_settings._explicit_settings:
-    #     dict_settings[key] = getattr(settings,key)
     return {'settings': django_settings}
+
+
+
+def alternate_seo_url(request):
+    alternate_url = dict()
+    path = request.path
+    url_parts = resolve( path )
+    url = path
+    cur_language = get_language()
+    for lang_code, lang_name in django_settings.LANGUAGES :
+        try:
+            activate(lang_code)
+            url = reverse( url_parts.view_name, kwargs=url_parts.kwargs )
+            alternate_url[lang_code] = django_settings.SITE_URL+url
+        finally:
+            activate(cur_language)
+
+    return {'alternate':alternate_url}
