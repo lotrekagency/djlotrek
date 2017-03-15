62f3edaad1fe6c840b188633f009bb50110d8751
pierdomenico reitano <pierdomenico@lotrek.it>
2017-03-15 22:48:42 +0100

    aggiunta templatetag per lo stato active del menu

--- templatetags/djlotrek_tags.py
+++ templatetags/djlotrek_tags.py
@@ -1,6 +1,8 @@
+import re
+
 from django import template
 import datetime
-
+from django.core.urlresolvers import reverse, NoReverseMatch
 
 register = template.Library()
 
@@ -13,3 +15,14 @@ def auto_update_year_range(start=None):
     if start:
         return '{0}-{1}'.format(start, current_year)
     return current_year
+
+@register.simple_tag(takes_context=True)
+def active(context, pattern_or_urlname):
+    try:
+        pattern = '^' + reverse(pattern_or_urlname)
+    except NoReverseMatch:
+        pattern = pattern_or_urlname
+    path = context['request'].path
+    if re.search(pattern, path):
+        return 'active'
+    return ''
