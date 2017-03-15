a52dd9d66ff7d9a29f6d635e5ca1a2a0584c267b
Andrea Stagi <stagi.andrea@gmail.com>
2017-03-15 04:05:43 +0100

    Add rosetta utils

---
+++ rosetta_utils.py
@@ -0,0 +1,15 @@
+# From: https://github.com/mbi/django-rosetta/issues/50
+# Gunicorn may work with --reload option but it needs
+# https://pypi.python.org/pypi/inotify package for performances
+
+from django.dispatch import receiver
+from rosetta.signals import post_save
+
+import time
+import os
+
+@receiver(post_save)
+def restart_server(sender, **kwargs):
+    os.system("./gunicorn.sh stop")
+    pass
+
