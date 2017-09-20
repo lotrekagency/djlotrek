4204d8f4708a6a54e7200390152e39bfc7f84357
sᴀʟᴠᴀᴛᴏʀᴇ <svt.busiello@gmail.com>
2017-09-20 11:31:20 +0200

    Test on auto_update_year_range

--- djlotrek/tests/test_templatetags.py
+++ djlotrek/tests/test_templatetags.py
@@ -1,12 +1,15 @@
 import os
 import mock
 
+import datetime
+
 from django.test import TestCase
 
-from djlotrek.templatetags.djlotrek_tags import absolute_url
+from djlotrek.templatetags.djlotrek_tags import absolute_url, auto_update_year_range
 
 from django.test import RequestFactory
 
+from mock import Mock, patch
 
 class TemplateTagsTestCase(TestCase):
 
@@ -44,3 +47,24 @@ class TemplateTagsTestCase(TestCase):
 
         abs_url = absolute_url(context, '/ciao/')
         self.assertEqual(abs_url, '/ciao/')
+        self.assertNotEqual(abs_url, '/')
+
+
+    @patch('djlotrek.templatetags.djlotrek_tags.datetime')
+    def test_auto_update_year_range(self,datetime_now):
+        datetime_now.datetime.now.return_value = datetime.date(2017,12,12)
+        current_year = 2017
+
+        tested_year = auto_update_year_range()
+        self.assertEqual(tested_year, current_year)
+        self.assertNotEqual(tested_year, current_year+1)
+
+        tested_year_start = auto_update_year_range('1995')
+
+        composed_year = "1995-"+str(current_year)
+        self.assertEqual(tested_year_start, composed_year)
+        self.assertNotEqual(tested_year_start, "-")
+
+
+
+
