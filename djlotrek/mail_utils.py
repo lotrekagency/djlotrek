104572006f16ced7b47924b1623f47dae9a99a94
Andrea Stagi <stagi.andrea@gmail.com>
2017-09-26 16:52:54 +0200

    New mail utils with files

--- djlotrek/mail_utils.py
+++ djlotrek/mail_utils.py
@@ -1,14 +1,19 @@
 from django.core.mail import send_mail as old_send_mail
+from django.core.mail import EmailMultiAlternatives
+
 from django.template import loader
 
 
 def send_mail(
     sender, receivers, subject, context={},
     template_html=None, template_txt=None,
-    plain_message='', fail_silently=False
+    plain_message='', fail_silently=False,
+    cc=[], bcc=[],files=[]
 ):
 
     extras = {}
+    plain_message = ''
+    html_text = ''
 
     if template_txt:
         template_txt = loader.get_template(template_txt)
@@ -16,10 +21,16 @@ def send_mail(
 
     if template_html:
         template_html = loader.get_template(template_html)
-        extras['html_message'] = template_html.render(context)
+        html_text = template_html.render(context)
 
-    old_send_mail(
-        subject, plain_message, sender,
-        receivers, fail_silently=fail_silently,
-        **extras
+    mail = EmailMultiAlternatives(
+        subject=subject, body=plain_message or html_text, 
+        from_email=sender, to=receivers,
+        cc=cc, bcc=bcc,
+        alternatives=((html_text or plain_message, 'text/html'),)
     )
+
+    for file in files:
+        if file['path']:
+            mail.attach_file(file['path'])
+    mail.send(fail_silently)
