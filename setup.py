f4611cc60c3dbce0a7780d65c15aea8c4fd52819
Andrea Stagi <stagi.andrea@gmail.com>
2017-09-26 16:54:51 +0200

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='djlotrek',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/lotrekagency/djlotrek',
     install_requires=[
         'Django >= 1.10',
