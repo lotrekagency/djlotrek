2d3aa1525961ed7cadb6c6293da15002d2ece274
yothinix <m@yothinix.com>
2017-10-01 14:08:08 +0700

    Add doctest for utils test :rocket:

--- djlotrek/tests/test_utils.py
+++ djlotrek/tests/test_utils.py
@@ -12,11 +12,11 @@ from djlotrek.aes import decode
 
 class UtilsTestCase(TestCase):
 
-    def setUp(self):
-        pass
-
     def test_group_objects_by_attribute(self):
-        """Our beloved get_host_url utility"""
+        """
+        group_objects_by_attribute utils use for group objects and attribute
+        together in a dictionary object
+        """
         class Human():
             name = ''
             age = 0
@@ -39,7 +39,10 @@ class UtilsTestCase(TestCase):
         self.assertEqual (len(grouped_objects[22]), 2)
 
     def test_order_dict_from_list(self):
-        """Our beloved get_host_url utility"""
+        """
+        order_dict_from_list utils pass dictionary and ordered key list
+        then return ordered dictionary
+        """
         my_dict = {
             'italian' : 'ciao',
             'english' : 'hello',
