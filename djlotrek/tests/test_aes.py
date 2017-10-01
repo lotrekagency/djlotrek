69b320b3dea16e78e3a00eb43111b4eadedfe159
yothinix <m@yothinix.com>
2017-10-01 13:44:20 +0700

    Add docstring for test_aes :rocket:
    - Also remove whitespace blank line as well

--- djlotrek/tests/test_aes.py
+++ djlotrek/tests/test_aes.py
@@ -15,11 +15,15 @@ class AESTestCase(TestCase):
         pass
 
     def test_aes(self):
-        """Our beloved get_host_url utility"""
+        """
+        aes.encode() module pass a raw string and return
+        AES encoded string and aes.decode() module pass
+        an encoded AES string and return decoded string
+        """
         enc = djlotrek.aes.encode('m.allegra92@gmail.com')
         dec = djlotrek.aes.decode(enc)
         self.assertEqual(dec, 'm.allegra92@gmail.com')
-        
+
         enc = djlotrek.aes.encode('stagi.andrea@gmail.com')
         dec = djlotrek.aes.decode(enc)
         self.assertEqual(dec, 'stagi.andrea@gmail.com')
