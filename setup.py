67ab4ca2145e173ed0ca263b78cd0c274abfb88a
bnznamco <gabriele.baldi.01@gmail.com>
2021-10-26 15:38:12 +0200

    chore: Use semantic versioning to publish to pypi

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from djlotrek import __version__
 
-
-setup(
-    name='djlotrek',
-    version='0.0.6',
-    url='https://github.com/lotrekagency/djlotrek',
-    install_requires=[
-        'pycryptodome >= 3.9.8',
-        'requests >= 2.18.4'
-    ],
-    description="Lotrek's beloved Django utilities library",
-    long_description=open('README.rst').read(),
-    license="MIT",
-    author="Lotrek",
-    author_email="dimmitutto@lotrek.it",
-    packages=find_packages(),
-    include_package_data=True,
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3'
-    ]
-)
+setup(version=__version__)
