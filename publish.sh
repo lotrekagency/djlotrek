1e150c03864101857f1f1db60e813a2f9db5ec42
bnznamco <gabriele.baldi.01@gmail.com>
2021-12-02 15:21:00 +0100

    chore: change build script and add semanti-release to dev dependencies

--- publish.sh
+++ publish.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-rm -rf dist build *.egg-info
-python setup.py sdist bdist_wheel
-sudo python -m twine upload dist/*
+
+
+if ! command -v semantic-release &> /dev/null
+then
+    echo "🙅 'semantic-release' seems not in PATH, maybe you are not in dev venv."
+    echo "Just create your venv and run 'pip install -r requirements-dev.txt'"
+    exit
+fi
+
+echo "Here what we are going to do:"
+
+semantic-release publish "$@" --noop
+
+while true; do
+    read -p "Do you wish to continue?" yn
+    case $yn in
+        [Yy]* ) semantic-release publish "$@"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
