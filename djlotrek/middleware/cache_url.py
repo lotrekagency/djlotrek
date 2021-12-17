55e7c8f58e1fdcaa84d2c5c2075c8ab3cf35c85e
bnznamco <gabriele.baldi.01@gmail.com>
2021-12-17 14:32:52 +0100

    test: fix formatting

--- djlotrek/middleware/cache_url.py
+++ djlotrek/middleware/cache_url.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.middleware.cache import UpdateCacheMiddleware
-import re 
+import re
 
 
 # This removes cache from certain url. The list of exceptions can be provided using CACHE_MIDDLEWARE_IGNORE setting also via regex.
@@ -9,7 +9,7 @@ class UrlIgnoreCacheMiddleware(UpdateCacheMiddleware):
         full_path = request.get_full_path()
         ignores = getattr(settings, 'CACHE_MIDDLEWARE_IGNORE', [])
         if isinstance(ignores, str):
-            ignores = [ignores] 
+            ignores = [ignores]
         for ignore in ignores:
             if re.match(ignore, full_path):
                 return response
