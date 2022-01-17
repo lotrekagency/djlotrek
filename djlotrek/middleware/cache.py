0c63d4512a32be59c5315a28e4a16772ca085b00
bnznamco <gabriele.baldi.01@gmail.com>
2022-01-17 18:45:38 +0100

    fix: fix cache middlewares and unify all middlewares under one master class

---
+++ djlotrek/middleware/cache.py
@@ -0,0 +1,6 @@
+from .cache_admin import CacheAdminMiddleware
+from .cache_header import CacheHeaderMiddleware
+from .cache_url import UrlIgnoreCacheMiddleware
+
+class FetchFromCacheMiddleware(CacheAdminMiddleware, CacheHeaderMiddleware, UrlIgnoreCacheMiddleware):
+    pass
--- djlotrek/middleware/cache_header.py
+++ djlotrek/middleware/cache_header.py
@@ -1,9 +1,9 @@
-from .cache_admin import CacheAdminMiddleware
+from django.middleware.cache import FetchFromCacheMiddleware
 
 
-class CacheHeaderMiddleware(CacheAdminMiddleware):
+class CacheHeaderMiddleware(FetchFromCacheMiddleware):
     def process_request(self, request):
-        if request.META.get('HTTP_DISABLE_CACHE', ''):
+        if request.META.get('HTTP_DISABLE_CACHE', False):
             request._cache_update_cache = False
             return None
         return super(CacheHeaderMiddleware, self).process_request(request)
